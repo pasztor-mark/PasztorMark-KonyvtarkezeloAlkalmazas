f95d33a210d9b7e10f798f268a57b59d50361014
Petrik Diak <diak@petrik.hu>
2024-12-04 16:22:07 +0100

    adapter minden bizonnyal adaptál

--- app/src/main/java/com/example/pasztormark_dolgozat/KonyvAdapter.java
+++ app/src/main/java/com/example/pasztormark_dolgozat/KonyvAdapter.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 
 import java.util.List;
 
@@ -13,19 +16,25 @@ public class KonyvAdapter extends ArrayAdapter<ListViewItem> {
     public KonyvAdapter(Context context, int resource, List<ListViewItem> objects) {
         super(context, resource, objects);
     }
+
+    @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.listviewitem, parent, false);
+        }
 
-        } ListViewItem item = getItem(position);
-        TextView cim = convertView.findViewById(R.id.cim);
-        TextView szerzo = convertView.findViewById(R.id.szerzo);
-        TextView oldalszam = convertView.findViewById(R.id.oldalszam);
+        ListViewItem item = getItem(position);
+        TextView cimDisplay = convertView.findViewById(R.id.cimDisplay);
+        TextView szerzoDisplay = convertView.findViewById(R.id.szerzoDisplay);
+        TextView oldalszamDisplay = convertView.findViewById(R.id.oldalszamDisplay);
 
-        item.setCim(cim.getText().toString());
-        item.setSzerzo(szerzo.getText().toString());
-        item.setOldalszam(Integer.parseInt(oldalszam.getText().toString()));return  convertView;
+        if (item != null) {
+            cimDisplay.setText(item.getCim());
+            szerzoDisplay.setText(item.getSzerzo());
+            oldalszamDisplay.setText(String.valueOf(item.getOldalszam()));
+        }
+
+            return convertView;
     }
-
 }
